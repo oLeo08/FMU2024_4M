b4d0d312352493dabf351c7a7b2ff7dd9ebde2b2
oLeo08 <leonardogregorio0033@gmail.com>
2024-03-27 10:43:54 -0300

    Projeto inicial aula4

--- app/src/main/java/br/fmu/aula/MainActivity.java
+++ app/src/main/java/br/fmu/aula/MainActivity.java
@@ -2,13 +2,30 @@ package br.fmu.aula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Private EditText editTextNumber;
+    Private EditText editTextNumber2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextNumber = findViewById(R.id.editTextNumber);
+        editTextNumber2 = findViewById(R.id.editTextNumber2);
+    }
+
+    public void somar(View View){
+        int x = Integer.parseInt(EditTextNumber.getText()toString);
+        int y = Integer.parseInt(EditTextNumber2.getText()toString);
+        int soma = x + y;
+        Intent Intent = new Intent(createPackageContext()this, MainActivity2.class);
+        Intent.putExtra("soma", soma);
+        startActivity(Intent);
+
+
     }
 }
---
+++ app/src/main/java/br/fmu/aula/MainActivity2.java
@@ -0,0 +1,21 @@
+package br.fmu.aula;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.textView);
+        int soma = getIntent().getIntExtra("soma", 0);
+        textView.setText(Integer.toString(soma));
+    }
+}
